4d0739ba2cc8a4e23519c34630f6d7d645b715f7
Ri4yy <maksimov.1913@list.ru>
2024-03-18 22:14:36 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,26 +1,108 @@
-let wrapper = document.querySelector('.ticker'),
-    marquee = document.querySelector('.ticker__text'),   
-    wrapperWidth = wrapper.offsetWidth,
-    marqueeWidth = marquee.scrollWidth;
-    
-document.querySelector('button').onclick = function() {
-  clearInterval(interval)
-}
-    
-function move() {
-  let currentTX = getComputedStyle(marquee).transform.split(',');
-  if( currentTX[4] === undefined ) {
-    currentTX = -1;
-  } else {
-    currentTX = parseFloat(currentTX[4]) - 1;
-  }
-  
-  if(-currentTX >= marqueeWidth) {
-    marquee.style.transform = 'translateX(' + wrapperWidth + 'px)';
-  
-  } else {
-    marquee.style.transform = 'translateX(' + currentTX + 'px)';
-  }
-}
+document.addEventListener("DOMContentLoaded", () => {
+    // Открытие мобильного меню
+    let toggle = false;
+    $('#openMenu').click((e) => {
+            $('#openMenu').toggleClass('btn-menu--open')
+            $('.menu').toggleClass('menu--open')
+            if(toggle) {
+                $('.header').css('background', 'transparent');
+                toggle = false;
+            } else {
+                $('.header').css('background', 'white');
+                toggle = true;
+            }
+            $('html').toggleClass('no-scroll');
+    }); 
 
-let interval = setInterval(move, 1);
+
+  // Фиксированная шапка при скролле
+  // $(window).on('load resize', () => {
+  //     let windows = $(window);
+  //     let nav = $('.header-container');
+  //     if($(window).width() < 1481) {
+  //         let headerHeight = $('.header-container').height()
+  //         headerHeight += 40;
+  //         let h = nav.offset().top;
+  //         windows.scroll(function(){
+  //             if (windows.scrollTop() > headerHeight){
+  //                 nav.addClass('header-fixed');
+  //                 $('main').css('margin-top', headerHeight + 'px');
+  //             } else {
+  //                 nav.removeClass('header-fixed');
+  //                 $('main').css('margin-top', '0');
+  //             }
+  //         });
+  //     } else {
+  //         windows.scroll(function(){
+  //             if (windows.scrollTop() > 140){
+  //                 nav.addClass('header-fixed');
+  //                 $('main').css('margin-top', '140px');
+  //             } else {
+  //                 nav.removeClass('header-fixed');
+  //                 $('main').css('margin-top', '0');
+  //             }
+  //         });
+  //         return 
+  //     }
+  // })
+
+  // function tabs(wrapperMain, wrapperTab, wrapperContent, activeTab, activeContent) {
+  //     $(wrapperTab).on('click', 'li:not('+activeTab+')', function () {
+  //         $(this)
+  //             .addClass(activeTab).siblings().removeClass(activeTab)
+  //             .closest(wrapperMain).find(wrapperContent).removeClass(activeContent).eq($(this).index()).addClass(activeContent);
+  //     });
+  // }
+  // tabs('.tabs', '.tabs__list', '.tabs__content', 'active-tab', 'active');
+
+  // Модальное окно
+  // function showModal(btnOpen, modalBody) {
+  //     btnOpen.click(function() {
+  //         modalBody.show( );
+  //         $('html').addClass('no-scroll');
+  //         return false;
+  //     });		
+   
+  //     $(document).keydown(function(e) {
+  //         if (e.keyCode === 27) {
+  //             e.stopPropagation();
+  //             modalBody.hide();
+  //             $('html').removeClass('no-scroll');
+  //         }
+  //     });
+      
+  //     modalBody.click(function(e) {
+  //         if ($(e.target).closest('.modal__wrapper').length == 0) {
+  //             $(this).hide();					
+  //             $('html').removeClass('no-scroll');
+  //         }
+  //     });
+  // }
+  // showModal($('.open-modal--consult'), $('.modal-consult'));
+})
+// let wrapper = document.querySelector('.ticker'),
+//     marquee = document.querySelector('.ticker__text'),   
+//     wrapperWidth = wrapper.offsetWidth,
+//     marqueeWidth = marquee.scrollWidth;
+    
+// document.querySelector('button').onclick = function() {
+//   clearInterval(interval)
+// }
+    
+// function move() {
+//   let currentTX = getComputedStyle(marquee).transform.split(',');
+//   if( currentTX[4] === undefined ) {
+//     currentTX = -1;
+//   } else {
+//     currentTX = parseFloat(currentTX[4]) - 1;
+//   }
+  
+//   if(-currentTX >= marqueeWidth) {
+//     marquee.style.transform = 'translateX(' + wrapperWidth + 'px)';
+  
+//   } else {
+//     marquee.style.transform = 'translateX(' + currentTX + 'px)';
+//   }
+// }
+
+// let interval = setInterval(move, 1);
---
+++ js/swiper.js
@@ -0,0 +1,76 @@
+const swiperPartners = new Swiper('.swiper-partners', {
+    spaceBetween: 60,
+    enabled: true,
+
+    breakpoints: {
+      320: {
+        allowTouchMove: true,
+        spaceBetween: 20,
+        slidesPerView: 2,
+        grid: {
+          rows: 1,
+          fill: "row",
+        }
+      },
+      480: {
+        allowTouchMove: true,
+        spaceBetween: 20,
+        slidesPerView: 3,
+        grid: {
+          rows: 1,
+          fill: "row",
+        }
+      },
+      768: {
+        allowTouchMove: true,
+        spaceBetween: 40,
+        slidesPerView: 4,
+        grid: {
+          rows: 1,
+          fill: "row",
+        }
+      },
+      1024: {
+        allowTouchMove: true,
+        slidesPerView: 4,
+        grid: {
+          rows: 2,
+          fill: "row",
+        }
+      },
+      1280: {
+        allowTouchMove: true,
+        slidesPerView: 5,
+        grid: {
+          rows: 2,
+          fill: "row",
+        }
+      },
+      1921: {
+        allowTouchMove: true,
+        slidesPerView: 6,
+        grid: {
+          rows: 2,
+          fill: "row",
+        }
+      },
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination-partners',
+    },
+});
+
+const swiperTicker = new Swiper(".swiper-ticker", {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  speed: 5000,
+  autoplay: {
+    delay: 0.01,
+  },
+  loop: true,
+  slidesPerView:'auto',
+  disableOnInteraction: true,
+  allowTouchMove:false,
+});
