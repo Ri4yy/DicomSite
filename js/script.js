12583f36a4812e84acc75446844aa8e8ebe20e6b
Ri4yy <maksimov.1913@list.ru>
2024-03-19 23:14:36 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -105,29 +105,3 @@ document.addEventListener("DOMContentLoaded", () => {
   // }
   // showModal($('.open-modal--consult'), $('.modal-consult'));
 })
-// let wrapper = document.querySelector('.ticker'),
-//     marquee = document.querySelector('.ticker__text'),   
-//     wrapperWidth = wrapper.offsetWidth,
-//     marqueeWidth = marquee.scrollWidth;
-    
-// document.querySelector('button').onclick = function() {
-//   clearInterval(interval)
-// }
-    
-// function move() {
-//   let currentTX = getComputedStyle(marquee).transform.split(',');
-//   if( currentTX[4] === undefined ) {
-//     currentTX = -1;
-//   } else {
-//     currentTX = parseFloat(currentTX[4]) - 1;
-//   }
-  
-//   if(-currentTX >= marqueeWidth) {
-//     marquee.style.transform = 'translateX(' + wrapperWidth + 'px)';
-  
-//   } else {
-//     marquee.style.transform = 'translateX(' + currentTX + 'px)';
-//   }
-// }
-
-// let interval = setInterval(move, 1);
